f23622062a014e563f9223a6545f47fa89ba9d84
ld <ldbzqyzz@gmail.com>
2021-07-07 16:10:22 +0800

    [add]git知识
    Update auto_sync.py
    
    昨晚的提交
    
    web 提交更新README.md

---
+++ auto_sync.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import git
+import os
+import subprocess
+import shutil
+
+print("hello world")
+print("haha哈哈")
+print("haha哈哈")
+
+import time
+
+while 1:
+      time.sleep(2)
+      print("world hello")
+      
