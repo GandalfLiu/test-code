5cd0d734d56a7fa04838c8a155ecd37933872565
ld <ldbzqyzz@gmail.com>
2021-07-07 16:10:22 +0800

    home-commit

--- auto_sync.py
+++ auto_sync.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 print("这是第二份的内容")
 print("这是第二份的内容")
 print("这是第二份的内容")
@@ -5,3 +6,11 @@ print("这是第二份的内容")
 print("这是第二份的内容")
 print("这是第二份的内容")
 print("five")
+
+import os
+import subprocess
+import shutil
+
+print("hello world")
+print("haha哈哈")
+print("haha哈哈")
