d35949dfc3d73a3c74ce23c4e1b3fb957fe2ead0
ld <ldbzqyzz@gmail.com>
2021-07-19 22:12:15 +0800

    third

--- auto_sync.py
+++ auto_sync.py
@@ -4,13 +4,4 @@ print("这是第二份的内容")
 print("这是第二份的内容")
 print("这是第二份的内容")
 print("这是第二份的内容")
-import time
-i = 0
-while 1:
-    if i >= 10:
-        break
-    i += 1
-    print("hehe")
-
-
-print("hello world")
+print("third")
