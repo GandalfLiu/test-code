9bf1bc0030a56d5dbcad55234fa7cf5ab1022dbd
ld <ldbzqyzz@gmail.com>
2021-07-19 21:20:21 +0800

    第二份

--- auto_sync.py
+++ auto_sync.py
@@ -1,17 +1,7 @@
-# -*- coding: utf-8 -*-
-
-import git
-import os
-import subprocess
-import shutil
-
-print("hello world")
-print("haha哈哈")
-print("haha哈哈")
-
-import time
-
-while 1:
-      time.sleep(2)
-      print("world hello")
+print("这是第二份的内容")
+print("这是第二份的内容")
+print("这是第二份的内容")
+print("这是第二份的内容")
+print("这是第二份的内容")
+print("这是第二份的内容")
       
